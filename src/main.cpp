9015d1af9a2e2a793f194ca8132b87d1ea35df99
Momentum93 <thomas.genius.meyer@gmail.com>
2024-06-26 14:00:58 +0200

    fix merge

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,5 @@
 #include <Arduino.h>
-<<<<<<< Updated upstream
 #include "server.h"
-
-#include "serial.h"
-
-// Default ip 192.168.4.1
-AccessPoint ap;
-// _Serial _serial;
-
-void setup() {
-  // _serial.register_pc_callback(
-  //   [](String str){_serial.write(str);});
-  // _serial.begin();
-
-  // ap.bind_print([](String str){_serial.write(str);});
-  ap.start();
-=======
-//#include "server.h"
-
 #include <ArrayList.h> 
 
 #define BAUDRATE 9600
@@ -26,8 +8,7 @@ ArrayList<String> L_fromPC;
 ArrayList<String> L_toPC;
 
 void setup() {
-  Serial.begin(9600);
->>>>>>> Stashed changes
+  Serial.begin(BAUDRATE);
 }
 
 
