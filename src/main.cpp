82b8696bc3237f3da67577240d030424a60376e3
Momentum93 <thomas.genius.meyer@gmail.com>
2024-06-26 13:59:25 +0200

    bidirectional serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+<<<<<<< Updated upstream
 #include "server.h"
 
 #include "serial.h"
@@ -14,8 +15,30 @@ void setup() {
 
   // ap.bind_print([](String str){_serial.write(str);});
   ap.start();
+=======
+//#include "server.h"
+
+#include <ArrayList.h> 
+
+#define BAUDRATE 9600
+
+ArrayList<String> L_fromPC;
+ArrayList<String> L_toPC;
+
+void setup() {
+  Serial.begin(9600);
+>>>>>>> Stashed changes
 }
+
+
 
 void loop() {
+  if (Serial.available()){
+    L_fromPC.add(Serial.readString());
+  }else if(L_toPC.size() > 0){
+    Serial.println(L_toPC.get(0));
+    L_toPC.remove(0);
+  }
 
 }
+
